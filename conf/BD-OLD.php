fd65c551066bcef18b66feef50fe450e720d4127
Kate Antunes <katekas@gmail.com>
2015-05-10 13:13:34 +0200

    Mudança da String de conexão
    Mudança da String de conexão

--- conf/BD-OLD.php
+++ conf/BD-OLD.php
@@ -1,16 +1,16 @@
 <?php
  function conn_mysql(){
 
-   $servidor = 'br-cdbr-azure-south-a.cloudapp.net';
+   $servidor = 'localhost';
    $porta = 3306;
-   $banco = "pucyearbookdb";
-   $usuario = "bbb6f752ae92eb";
-   $senha = "81ea0a91";
-
+   $banco = "daw_yearbook";
+   $usuario = "yearbook";
+   $senha = "year1234";
+   
       $conn = new PDO("mysql:host=$servidor;
 	                   port=$porta;
-					   dbname=$banco",
-					   $usuario,
+					   dbname=$banco", 
+					   $usuario, 
 					   $senha,
 					   array(PDO::ATTR_PERSISTENT => true)
 					   );
